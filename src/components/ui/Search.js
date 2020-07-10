afe3db3fe8042f73b384114759fd0cdd0f999925
atchoi-delacruz <atchoi.delacruz@gmail.com>
2020-07-10 16:45:31 +0800

    added Search component

--- src/components/ui/Search.js
+++ src/components/ui/Search.js
@@ -1,10 +1,25 @@
 import React, { useState } from 'react'
 
-const Search = () => {
-    return (
-        <div>
+const Search = ({ getQuery }) => {
+    const [text, setText] = useState('');
+    const onChange = (q) => {
+        setText(q);
+        getQuery(q);
+    }
 
-        </div>
+    return (
+        <section className='search'>
+            <form>
+                <input
+                    type='text'
+                    className='form-control'
+                    placeholder='Search Characters'
+                    value={text}
+                    onChange={(e) => onChange(e.target.value)}
+                    autoFocus
+                />
+            </form>
+        </section>
     )
 }
 
